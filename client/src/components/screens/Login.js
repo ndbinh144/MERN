9ac736d90925357a51329c93f37e41d460ce07ed
bean <beannguyen@gamil.com>
2020-07-05 14:32:15 +0700

    create signin signup pageui

--- client/src/components/screens/Login.js
+++ client/src/components/screens/Login.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
+import {Link} from 'react-router-dom';
 
 const Login = () => {
-    return (
-        <h1>Login</h1>
-    );
-}
+  return (
+    <div className="mycard">
+      <div className="card auth-card input-field">
+        <h2>Instagram</h2>
+        <input type="text" placeholder="Email" />
+        <input type="text" placeholder="Password" />
+        <button class="btn waves-effect waves-light #64b5f6 blue lighten-2">Login</button>
+        <h5>
+          <Link to="/signup">Don't have an account?</Link>
+        </h5>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+import {Link} from 'react-router-dom';
 
 const Signup = () => {
-    return (
-        <h1>Signup</h1>
-    );
-}
+  return (
+    <div className="mycard">
+      <div className="card auth-card input-field">
+        <h2>Instagram</h2>
+        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Email" />
+        <input type="text" placeholder="Password" />
+        <button class="btn waves-effect waves-light #64b5f6 blue lighten-2">SignUp</button>
+        <h5>
+            <Link to="/login">Already have an account?</Link>
+        </h5>
+      </div>
+    </div>
+  );
+};
 
 export default Signup;
