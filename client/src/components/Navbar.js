0605e924ba48139be0ba3fad020b960efd2167dd
bean <bean@test-cube.com>
2020-07-05 20:37:13 +0700

    fetch post and show that on home page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,26 +1,42 @@
-import React from "react";
+import React, {useContext} from "react";
 import {Link} from 'react-router-dom';
+import {UserContext} from '../App';
 
 const NavBar = () => {
-  return (
-    <nav>
-      <div className="nav-wrapper white">
-        <Link to="/" className="brand-logo left">
-          Instagram
-        </Link>
-        <ul id="nav-mobile" className="right">
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/signup">SignUp</Link>
-          </li>
+  const {state, dispatch} = useContext(UserContext);
+  const RenderList = () => {
+    if (state) {
+      return (
+        <>
           <li>
             <Link to="/profile">Profile</Link>
           </li>
           <li>
             <Link to="/create">Create Post</Link>
           </li>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/signup">SignUp</Link>
+          </li>
+        </>
+      );
+    }
+  }
+  return (
+    <nav>
+      <div className="nav-wrapper white">
+        <Link to={state ? "/" : "/login"} className="brand-logo left">
+          Instagram
+        </Link>
+        <ul id="nav-mobile" className="right">
+          <RenderList />
         </ul>
       </div>
     </nav>
