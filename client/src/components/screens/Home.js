d3c674bed38c5a488d0584eca5a941404e33fd2f
bean <bean@test-cube.com>
2020-07-06 21:30:55 +0700

    like and unlike

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,8 +1,10 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
+import {UserContext} from '../../App';
 
 const Home = () => {
     const [data, setData] = useState([]);
+    const {state, dispatch} = useContext(UserContext);
     useEffect(() => {
         fetch('/allpost', {
             headers: {
@@ -16,6 +18,59 @@ const Home = () => {
             console.log(err);
         });
     }, []);
+
+    const likePost = (id) => {
+        fetch('/like', {
+            method: 'put',
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + localStorage.getItem("jwt"),
+            },
+            body: JSON.stringify({
+                postId: id
+            })
+        }).then(rs => rs.json())
+        .then(result => {
+            const newData = data.map(item => {
+                if (item._id === result._id) {
+                    return result;
+                } else {
+                    return item;
+                }
+            });
+            setData(newData);
+        }).catch(err => {
+            console.log(err);
+            
+        })
+    }
+
+    const unLikePost = (id) => {
+        fetch('/unlike', {
+            method: 'put',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem("jwt"),
+            },
+            body: JSON.stringify({
+                postId: id
+            })
+        }).then(rs => rs.json())
+        .then(result => {
+            const newData = data.map(item => {
+                if (item._id === result._id) {
+                    return result;
+                } else {
+                    return item;
+                }
+            });
+            setData(newData);
+        }).catch(err => {
+            console.log(err);
+            
+        })
+    }
+
     return (
         <div className="home">
             {
@@ -28,6 +83,12 @@ const Home = () => {
                             </div>
                             <div className="card-content">
                                 <i className="material-icons" style={{color: "red"}}>favorite</i>
+                                {
+                                    item.likes.includes(state._id)
+                                    ? <i className="material-icons" onClick={() => {unLikePost(item._id)}}>thumb_down</i>
+                                    : <i className="material-icons" onClick={() => {likePost(item._id)}}>thumb_up</i>
+                                }
+                                <h6>{item.likes.length} likes</h6>
                                 <h6>{item.title}</h6>
                                 <p>{item.body}</p>
                                 <input type="text" placeholder="add a comment"/>
--- server/models/post.js
+++ server/models/post.js
@@ -1,22 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema.Types;
 const postSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  postedBy: {
+    type: ObjectId,
+    ref: "User",
+  },
+  likes: [
+    {
+      type: ObjectId,
+      ref: "User",
     },
-    body: {
-        type: String,
-        required: true,
-    },
-    photo: {
-        type: String,
-        required: true,
-    },
-    postedBy: {
-        type: ObjectId,
-        ref: 'User'
-    }
-})
+  ],
+});
 
-mongoose.model('Post', postSchema);
+mongoose.model("Post", postSchema);
